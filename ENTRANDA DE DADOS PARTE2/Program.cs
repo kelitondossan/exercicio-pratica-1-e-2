9b1379d186d397fe88ad17d7ef3cb7daae150cfb
keliton.santos <keliton.santos@ESTNTB003>
2022-02-10 15:56:06 -0300

    Adicionar arquivos de projeto.

---
+++ ENTRANDA DE DADOS PARTE2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ENTRANDA_DE_DADOS_PARTE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(),CultureInfo.CurrentCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.CurrentCulture);
+
+
+            Console.WriteLine("voce digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.CurrentCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.CurrentCulture));
+
+        }
+    }
+}
---
+++ EXERCICIO RESOLVIDO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace EXERCICIO_RESOLVIDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com  o preço de um produto");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu ulytimo nome, idade e altura ( mesma linha): ");
+
+            
+
+            string[] vs = Console.ReadLine().Split(' ');
+        
+            string sobrenome = vs[0];
+            int idade = int.Parse(vs[1]);
+            double altura = double.Parse(vs[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);            
+            Console.WriteLine(preco.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
